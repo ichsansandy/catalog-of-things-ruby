32e7df1f8706d071e88de87ab833b59cfb2e6327
moFuhidy <mo7ammed9290@gmail.com>
2023-09-11 12:01:10 +0300

    Implement the class Item methods

--- lib/item/item.rb
+++ lib/item/item.rb
@@ -1,12 +1,18 @@
-class Item 
+require 'date'
 
-    def initialize()
+class Item 
+    def initialize(id, publish_date, archived)
       @id = Random.rand(1..1000)
-      @publish_date = Date.now
-      @archived = false
+      @publish_date = Date.parse(publish_date)
+      @archived = archived
     end
     
-    def can_be_archived?()
+    def can_be_archived?
+        (Date.today.year - @publish_date.year) > 10
     end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
 end
