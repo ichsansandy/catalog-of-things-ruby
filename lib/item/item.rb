a8aa4e257fcd3c2485afe0c1ad9ce08207ce2629
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-11 16:41:46 +0700

    Complete main.rb function

--- lib/item/item.rb
+++ lib/item/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item 
-    def initialize(id, publish_date, archived)
+    def initialize(id, publish_date, archived: false)
       @id = Random.rand(1..1000)
       @publish_date = Date.parse(publish_date)
       @archived = archived
@@ -11,15 +11,18 @@ class Item
         (Date.today.year - @publish_date.year) > 10
     end
 
-  def move_to_archive
-    @archived = true if can_be_archived?
-  end
-  def genre = (genre)
-    @genre = genre
-  end
-  def label = (label)
-    @label = label
-  end
+    def move_to_archive
+        @archived = true if can_be_archived?
+    end
+
+    def genre = (genre)
+        @genre = genre
+    end
+
+    def label = (label)
+        @label = label
+    end
+
   def author = (author)
     @author = author
   end
